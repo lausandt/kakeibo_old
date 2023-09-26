b75ad4f289513a2e1d14032d684a6caedec3a9b2
unknown <laurens.sandt@protonmail.com>
2023-09-26 12:54:16 +0200

    preparing last part before coding

--- kakeibo/__main__.py
+++ kakeibo/__main__.py
@@ -1,7 +1,81 @@
-import sys
+from pathlib import Path
+from typing import Any, Optional
 
-from kakeibo.kakeibo import fib
+from fastapi import APIRouter, FastAPI, HTTPException, Query, Request
+from fastapi.templating import Jinja2Templates
+
+from kakeibo.recipe_data import RECIPES
+from kakeibo.schemas.schemas import Recipe, RecipeCreate, RecipeSearchResults
+
+BASE_PATH = Path(__file__).resolve().parent
+TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "templates"))
+
+app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
+api_router = APIRouter()
+
+
+@api_router.get("/", status_code=200)
+def root(request: Request) -> Any:
+    """
+    Root Get
+    """
+    return TEMPLATES.TemplateResponse(
+        "index.html",
+        {"request": request, "recipes": RECIPES},
+    )
+
+
+@api_router.get("/recipe/{recipe_id}", status_code=200, response_model=Recipe)
+def fetch_recipe(*, recipe_id: int) -> dict:
+    """
+    Fetch a single recipe by ID
+    """
+    result = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
+    if not result:
+        raise HTTPException(
+            status_code=404, detail=f"Recipe with ID {recipe_id} not found"
+        )
+    return result[0]
+
+
+@api_router.get("/search/", status_code=200, response_model=RecipeSearchResults)
+def search_recipes(
+    *,
+    keyword: Optional[str] = Query(None, min_length=3, example="chicken"),
+    max_results: Optional[int] = 10,
+) -> dict:
+    """
+    Search for recipes based on label keyword
+    """
+    if not keyword:
+        return {"results": RECIPES[:max_results]}
+    return {
+        "results": [
+            recipe for recipe in RECIPES if keyword.lower() in recipe["label"].lower()  # type: ignore
+        ][:max_results]
+    }
+
+
+@api_router.post("/recipe/", status_code=201, response_model=Recipe)
+def create_recipe(*, recipe_in: RecipeCreate) -> Recipe:
+    """
+    Create a new recipe (in memory only)
+    """
+    new_entry_id = len(RECIPES) + 1
+    recipe_entry = Recipe(
+        id=new_entry_id,
+        label=recipe_in.label,
+        source=recipe_in.source,
+        url=recipe_in.url,
+    )
+    RECIPES.append(recipe_entry.model_dump())
+
+    return recipe_entry
+
+
+app.include_router(api_router)
 
 if __name__ == "__main__":
-    n = int(sys.argv[1])
-    print(fib(n))
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
--- kakeibo/kakeibo.py
+++ kakeibo/kakeibo.py
@@ -1,26 +1,17 @@
-# from functools import cache
-#
-# @cache
-# def fib(n: int) -> int:
-#     if n < 1:
-#         return 0
-#     if n == 1:
-#         return n
-#     else:
-#         return fib(n - 1) + fib(n - 2)
 from typing import Iterator
 
 
 def fibonacci(n: int) -> Iterator[int]:
-    idx = -1
+    idx = 0
     a, b = 0, 1
-    while idx < n:
+    while idx <= n:
         yield a
         a, b = b, a + b
         idx += 1
 
 
 def fib(n: int) -> int:
+    """function that returns the nth fibonacci number"""
     if n < 1:
         return 0
     return [f for f in fibonacci(n)][n]
